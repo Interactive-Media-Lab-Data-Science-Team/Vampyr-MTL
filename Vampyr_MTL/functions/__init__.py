0f17f7ce2580b8e781a2aec6511a625b335335c0
DaPraxis <haoyanjiang0709@gmail.com>
2020-06-25 19:11:42 -0400

    update to pypi test 0.0.2

---
+++ Vampyr_MTL/functions/__init__.py
@@ -0,0 +1,7 @@
+from .MTL_Cluster_Least_L21 import MTL_Cluster_Least_L21
+from .MTL_Least_L21 import MTL_Least_L21
+from .MTL_Softmax_L21_hinge import MTL_Softmax_L21
+from .Ordinal_Logistic_L21 import MTL_Logistic_L21
+
+__all__ = ['MTL_Cluster_Least_L21', 'MTL_Least_L21', 'MTL_Softmax_L21', 'grid_to_graph', 'text',
+           'MTL_Logistic_L21']
