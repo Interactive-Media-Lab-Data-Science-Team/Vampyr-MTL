8f6afd0f1ac09d5aef43a4920abcb46370e4605d
DaPraxis <haoyanjiang0709@gmail.com>
2020-06-27 17:14:15 -0400

    doc updates

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -33,23 +33,24 @@ release = '0.0.5'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinxcontrib.napoleon',
+    # 'sphinxcontrib.napoleon',
+    'recommonmark'
 ]
 
 # Napoleon settings
-napoleon_google_docstring = True
-napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = False
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
-napoleon_use_keyword = True
-napoleon_custom_sections = None
+# napoleon_google_docstring = True
+# napoleon_numpy_docstring = False
+# napoleon_include_init_with_doc = False
+# napoleon_include_private_with_doc = False
+# napoleon_include_special_with_doc = False
+# napoleon_use_admonition_for_examples = False
+# napoleon_use_admonition_for_notes = False
+# napoleon_use_admonition_for_references = False
+# napoleon_use_ivar = False
+# napoleon_use_param = True
+# napoleon_use_rtype = True
+# napoleon_use_keyword = True
+# napoleon_custom_sections = None
 
 autodoc_default_flags = ['members']
 # Add any paths that contain templates here, relative to this directory.
