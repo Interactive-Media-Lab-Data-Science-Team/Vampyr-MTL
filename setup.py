78053256b3f875db90645dd55a6b4ecc62539aa4
DaPraxis <haoyanjiang0709@gmail.com>
2020-09-04 01:13:56 -0400

    package release, 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MD-MTL",
-    version="0.0.6",
+    version="0.0.7",
     author="Max Jiang",
     author_email="haoyanhy.jiang@mail.utoronto.ca",
     description="An Ensemble Med-Multi-Task Learning Package",
